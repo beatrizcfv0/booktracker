a4c18c3067a229a6ba10b3169336335dbf203221
Beatriz Cejas-Fuentes Valverde <beatrizcejas@MacBook-Pro-de-Beatriz.local>
2024-03-22 13:45:05 +0000

    Basic structure of home page

--- lib/main.dart
+++ lib/main.dart
@@ -15,51 +15,96 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 16, 100, 142),
-          title: const Text("reader",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+          backgroundColor: Color.fromARGB(255, 16, 100, 142),
+          title: const Text(
+            "reader",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 20),
-            child: Icon(
-            Icons.person_outlined,
+          actions: [
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Icon(
+                Icons.person_outlined,
               ),
             ),
           ],
         ),
-        
-        backgroundColor: Color.fromARGB(255, 247, 244, 232),
-        body: Column(
+        backgroundColor: Color.fromARGB(255, 255, 253, 247),
+        body: Stack(
           children: [
-            Container(
-              height: 400,
-              width: 400,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 16, 100, 142),
-                borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(35), 
-                bottomRight: Radius.circular(35))
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 400,
+                width: 400,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 16, 100, 142),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(35),
+                    bottomRight: Radius.circular(35),
+                  ),
+                ),
+              ),
             ),
-          ),
-            ElevatedButton(
-              onPressed: () {
-                // Acción que se realiza al presionar el botón
-              },
-              style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromRGBO(41, 169, 122, 1), // Background color
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15), // Button border shape
+            Positioned(
+              top: 375,
+              left: 100,
+              right: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Action performed when button is pressed
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(41, 169, 122, 1),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                ),
+                child: Text('Submit progress',style: TextStyle(color: Colors.white)),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),              
+            ),
+            Positioned(
+              left: 15,
+              right: 15,
+              bottom: 40,
+              child: Container(
+                height: 150,
+                width: 360,
+                padding: EdgeInsets.only(left: 20, top: 15),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(217, 218, 218, 218),
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                ),
+                child: Text('Reading Goals',
+                ),
               ),
-              child: Text('Submit progress', style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color.fromARGB(255, 255, 253, 247),
+          elevation: 0,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book_rounded),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '',
+          ),
+        ],
+      ),
       ),
     );
   }
